c018b49c4deb7dcbd4c05f4008fa786643f08d1e
nkaberov <nkaberov@webim.ru>
2024-09-16 13:54:46 +0300

    Version 1.2.5

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["WebimMobileWidget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/webim/webim-client-sdk-ios.git", exact: "3.41.6"),
+        .package(url: "https://github.com/webim/webim-client-sdk-ios.git", exact: "3.41.11"),
         .package(url: "https://github.com/kean/Nuke.git", from: "12.0.0"),
         .package(url: "https://github.com/evgenyneu/Cosmos.git", from: "25.0.1"),
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
